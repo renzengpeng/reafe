c4854afd8381a6925a43c30d60ec098ac385deb6
rzp <992321068@qq.com>
2017-07-13 22:59:31 +0800

    add

---
+++ caches/caches_commons/caches_data/common.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  'admin_email' => 'phpcms@phpcms.cn',
+  'maxloginfailedtimes' => '8',
+  'minrefreshtime' => '2',
+  'mail_type' => '1',
+  'mail_server' => 'smtp.qq.com',
+  'mail_port' => '25',
+  'category_ajax' => '0',
+  'mail_user' => 'healstec@foxmail.com',
+  'mail_auth' => '1',
+  'mail_from' => 'healstec@foxmail.com',
+  'mail_password' => '',
+  'errorlog_size' => '20',
+);
+?>
---
+++ caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => '127.0.0.1',
+		'port' => 3306,
+		'database' => 'reafe',
+		'username' => 'root',
+		'password' => '123456',
+		'tablepre' => 'hl_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
