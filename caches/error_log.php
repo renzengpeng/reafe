3fc766a218b839d568e40da0a2ad438bff8e70f6
rzp <992321068@qq.com>
2017-07-17 00:33:26 +0800

    中文图片

--- caches/error_log.php
+++ caches/error_log.php
@@ -3214,3 +3214,21 @@
 <?php exit;?>07-16 11:25:01 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
 <?php exit;?>07-16 11:25:04 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
 <?php exit;?>07-16 11:25:10 | 2 | fsockopen():                   | phpcms\libs\classes\http.class.php | 84
+<?php exit;?>07-16 13:40:25 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:40:25 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>07-16 13:40:25 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:40:25 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:40:25 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>07-16 13:45:02 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>07-16 13:45:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:45:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:45:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>07-16 13:45:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>07-16 13:45:02 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
